5d54e34f5da201359a558320d62c1048d8545e5d
Ben bui <btb4516@g.rit.edu>
2024-05-22 14:11:16 -0400

    hero

--- motherly_home_care/index.php
+++ motherly_home_care/index.php
@@ -1,7 +1,8 @@
 <?php include('nav.php'); ?>
 <main>
-    <h1>Welcome to Motherly Home Care</h1>
-    <h2>When it's family...</h2>
-    <p>Motherly is a home health care agency for adults and seniors living in the Philadelphia area. We offer companionship service and assistance with activities of daily living. This includes dressing, bathing, meal preparation, medication reminders, and many other household tasks as well as other services personally tailored for your care.</p>
+    <div class="hero">
+        <h1>Motherly Home Care</h1>
+        <h2>When it's family...</h2>
+        <p>Motherly is a home health care agency for adults and seniors living in the Philadelphia area. We offer companionship service and assistance with activities of daily living. This includes dressing, bathing, meal preparation, medication reminders, and many other household tasks as well as other services personally tailored for your care.</p>
+    </div>
 </main>
-<script src="js/script.js"></script>

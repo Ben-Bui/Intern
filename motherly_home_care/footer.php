844e1dba42e4719b7e81e212bce352b444eece11
Ben bui <btb4516@g.rit.edu>
2024-05-22 14:33:06 -0400

    translation

--- motherly_home_care/footer.php
+++ motherly_home_care/footer.php
@@ -1,8 +1,9 @@
 <footer>
     <div class="footer-content">
         <p>Address: 1019 South 8th Street Philadelphia, PA 19147</p>
-        <p>Person to contact: Mary Phan RN, BSN (Nurse)</p>
+        <p>Contact: Mary Phan RN, BSN (Nurse)</p>
         <p>Phone: (215) 900-5880</p>
         <p>Email: <a href="mailto:marykphan@gmail.com" style="color: white;">marykphan@gmail.com</a></p>
     </div>
 </footer>
+<script src="js/translation.js"></script>

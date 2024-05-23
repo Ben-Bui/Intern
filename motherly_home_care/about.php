de211a968640e932953ecaf42a4ffae5ca89bd69
Ben bui <btb4516@g.rit.edu>
2024-05-23 13:02:13 -0400

    revert

--- motherly_home_care/about.php
+++ motherly_home_care/about.php
@@ -1,7 +1,7 @@
 <?php include('nav.php'); ?>
 <main>
-    <h1 data-translate="aboutTitle">About Us</h1>
-    <h2 data-translate="ourMission">Our Mission</h2>
-    <p data-translate="missionDescription">Your safety and quality care is our mission. Providing our clients with the best health options, we strive to make the quality of life richer, healthier, and happier.</p>
+    <h1>About us</h1>
+    <h2>Our Mission</h2>
+    <p>Your safety and quality care is our mission. Providing our clients with the best health options, we strive to make the quality of life richer, healthier, and happier.</p>
 </main>
 <?php include('footer.php'); ?>

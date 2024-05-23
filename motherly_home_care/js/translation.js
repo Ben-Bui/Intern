e932be95a329fb0cdeadf323783b19e2e7277192
Ben bui <btb4516@g.rit.edu>
2024-05-23 13:38:50 -0400

    update

--- motherly_home_care/js/translation.js
+++ motherly_home_care/js/translation.js
@@ -1,18 +1,17 @@
 document.getElementById('translateBtn').addEventListener('click', function() {
-    const elementsToTranslate = document.querySelectorAll('main h1, main h2, main p, form label, form button');
+    const elementsToTranslate = document.querySelectorAll('main h1, main h2, main h3, main p');
     const currentLanguage = this.getAttribute('data-language');
     
     elementsToTranslate.forEach(element => {
         switch (element.textContent.trim()) {
-            // English to Vietnamese translations
-            case 'Welcome to Motherly Home Care':
-                element.textContent = 'Chào mừng đến với Motherly Home Care';
+            case 'Motherly Home Care':
+                element.textContent = 'Chăm sóc tại nhà Motherly';
                 break;
             case 'When it\'s family...':
                 element.textContent = 'Khi là gia đình...';
                 break;
             case 'Motherly is a home health care agency for adults and seniors living in the Philadelphia area. We offer companionship service and assistance with activities of daily living. This includes dressing, bathing, meal preparation, medication reminders, and many other household tasks as well as other services personally tailored for your care.':
-                element.textContent = 'Motherly Home Care là dịch vụ chăm sóc sức khỏe tại nhà cho người lớn và người cao niên sống trong khu vực Philadelphia.';
+                element.textContent = 'Motherly là một công ty chăm sóc sức khỏe tại nhà cho người lớn và người cao tuổi sống trong khu vực Philadelphia. Chúng tôi cung cấp dịch vụ bạn bè và hỗ trợ với các hoạt động hàng ngày. Điều này bao gồm việc mặc quần áo, tắm, chuẩn bị bữa ăn, nhắc nhở đường dùng thuốc, và nhiều công việc hộ gia đình khác cũng như các dịch vụ khác được cá nhân hóa cho sự chăm sóc của bạn.';
                 break;
             case 'About Us':
                 element.textContent = 'Về Chúng Tôi';
@@ -21,13 +20,13 @@ document.getElementById('translateBtn').addEventListener('click', function() {
                 element.textContent = 'Nhiệm Vụ Của Chúng Tôi';
                 break;
             case 'Your safety and quality care is our mission. Providing our clients with the best health options, we strive to make the quality of life richer, healthier, and happier.':
-                element.textContent = 'Chúng tôi hứa sẽ chăm sóc bạn, người bệnh của gia đình bạn, cũng như chính chúng tôi đang chăm sóc cho người thân của mình với lòng kiên nhẫn, thấu hiểu và tôn trọng.';
+                element.textContent = 'An toàn và chất lượng chăm sóc của bạn là nhiệm vụ của chúng tôi. Cung cấp cho khách hàng của chúng tôi các lựa chọn sức khỏe tốt nhất, chúng tôi cố gắng làm cho chất lượng cuộc sống của bạn giàu có, khỏe mạnh và hạnh phúc hơn.';
                 break;                    
             case 'Our Services':
                 element.textContent = 'Dịch Vụ Của Chúng Tôi';
                 break;
             case 'Explore the wide range of home care services we offer to make your life easier.':
-                element.textContent = 'Khám phá các dịch vụ chăm sóc tại nhà mà chúng tôi cung cấp để làm cho cuộc sống của bạn dễ dàng hơn.';
+                element.textContent = 'Khám phá loạt các dịch vụ chăm sóc tại nhà rộng lớn mà chúng tôi cung cấp để làm cuộc sống của bạn dễ dàng hơn.';
                 break;
             case 'Contact Us':
                 element.textContent = 'Liên Hệ Chúng Tôi';
@@ -53,18 +52,6 @@ document.getElementById('translateBtn').addEventListener('click', function() {
             case 'Apply':
                 element.textContent = 'Đăng Ký';
                 break;
-            case 'address':
-                element.textContent = 'Address: 1019 South 8th Street Philadelphia, PA 19147';
-                break;
-            case 'contact':
-                element.textContent = 'Contact: Mary Phan RN, BSN (Nurse)';
-                break;
-            case 'phone':
-                element.textContent = 'Phone: (215) 900-5880';
-                break;
-            case 'email':
-                element.innerHTML = 'Email: <a href="mailto:marykphan@gmail.com" style="color: white;">marykphan@gmail.com</a>';
-                break;
             default:
                 // No change if not matched
                 element.textContent = element.textContent;
@@ -74,8 +61,10 @@ document.getElementById('translateBtn').addEventListener('click', function() {
     // Change the button text to "English" when content is translated to Vietnamese
     if (currentLanguage === 'en') {
         this.textContent = 'English';
+        this.setAttribute('data-language', 'vi');
     } else {
         // Revert button text to "Tiếng Việt" when content is reverted to English
         this.textContent = 'Tiếng Việt';
+        this.setAttribute('data-language', 'en');
     }
 });

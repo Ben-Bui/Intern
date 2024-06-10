cf06f64c221d96da0e170440ed34979d332e290d
Ben bui <btb4516@g.rit.edu>
2024-06-10 16:59:07 -0400

    update

--- motherly_home_care/services.php
+++ motherly_home_care/services.php
@@ -4,7 +4,7 @@
     <div id="englishContent">
         <h1>Our Promise</h1>
         <p>We promise to care for you as any mother would care for her own: patience, kindness, and respect are our priorities. We aim to help ease the burden of personal care tasks & household chores in order for our clients to maintain independence in the comfort of home. By eliminating these daily challenges, our clients are able to spend more time on the activities that bring them joy and purpose. We believe that a great relationship begins with taking time to get to know each other. That's why we ask our clients about daily habits, interests, spoken languages, and cultural traditions before we match them with one of our caregivers.</p>
-        <h2>Our Services<h2>
+        <h2>Our Services</h2>
             <ul>
                 <li>Care in the comfort of your home or elsewhere, our custom services are provided in many settings.</li>
                 <li>We can run errands and go to appointments with you, as well as setting reminder for you.</li>
@@ -15,7 +15,7 @@
     <div id="vietnameseContent" style="display: none;">
         <h1>Lời Hứa</h1>
         <p>Tại Motherly Home Care, sự an toàn và hài lòng là trên hết. Chúng tôi dành thêm thời gian để đảm bảo các nhu cầu của bạn được đáp ứng, đồng thời còn vượt lên trên để bạn có thể tận hưởng cuộc sống theo cách bạn muốn. Bạn sẽ luôn được đối xử như một gia đình tại Motherly Home Care.</p>
-        <h2>Chăm Sóc Yêu Thương<h2>
+        <h2>Chăm Sóc Yêu Thương</h2>
             <ul>
                 <li>Giúp bạn vào nhà vệ sinh và thay quân áo.</li>
                 <li>Giúp bạn uống thuốc theo chỉ định của bác sĩ điều trị.</li>

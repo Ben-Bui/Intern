e265b8fabfe32222e8f77ea7668ca9ea7275c976
Ben bui <btb4516@g.rit.edu>
2024-06-13 17:20:05 -0400

    change format and picture

--- motherly_home_care/about.php
+++ motherly_home_care/about.php
@@ -1,15 +1,22 @@
 <?php include('nav.php'); ?>
 <main>
     <div id="englishContent">
-        <h1>About Us</h1>
-        <figure>
-        <img class="picture" src="img/elder_care.jpg" id="pic" alt="elder couple" >
-    </figure> 
+    <figure class="hero">
+        <img class="picture" src="img/elder_care.jpg" id="pic" alt="elder couple">
+        <figcaption>
+            <h1>About Us</h1>
+        </figcaption>
+    </figure>
         <h2>Our Mission</h2>
         <p>Your safety and quality care is our mission. Providing our clients with the best health options, we strive to make the quality of life richer, healthier, and happier.</p>
     </div>
     <div id="vietnameseContent" style="display: none;">
-        <h1>Về Chúng Tôi</h1>
+        <figure class="hero">     
+            <img class="picture" src="img/elder_care.jpg" id="pic" alt="elder couple">
+        <figcaption>
+            <h1>Về Chúng Tôi</h1>
+        </figcaption>
+        </figure>
         <h2>Nhiệm Vụ Của Chúng Tôi</h2>
         <p>Chúng tôi hứa sẽ chăm sóc bạn, người bệnh của gia đình bạn, cũng như chính chúng tôi đang chăm sóc cho người thân của mình với lòng kiên nhẫn, thấu hiểu và tôn trọng.
 </p>
--- motherly_home_care/services.php
+++ motherly_home_care/services.php
@@ -2,7 +2,13 @@
 
 <main>
     <div id="englishContent">
-        <h1>Our Promise</h1>
+    <figure class="hero">     
+            <img class="picture" src="img/services.jpg" id="pic" alt="elder couple">
+        <figcaption>
+            <h1>Services</h1>
+        </figcaption>
+    </figure>
+        <h2>Our Promise</h2>
         <p>We promise to care for you as any mother would care for her own: patience, kindness, and respect are our priorities. We aim to help ease the burden of personal care tasks & household chores in order for our clients to maintain independence in the comfort of home. By eliminating these daily challenges, our clients are able to spend more time on the activities that bring them joy and purpose. We believe that a great relationship begins with taking time to get to know each other. That's why we ask our clients about daily habits, interests, spoken languages, and cultural traditions before we match them with one of our caregivers.</p>
         <h2>Our Services</h2>
             <ul>
@@ -32,7 +38,13 @@
                 <li>We provide care with the utmost respect and compassion.</li>
     </div>
     <div id="vietnameseContent" style="display: none;">
-        <h1>Lời Hứa</h1>
+    <figure class="hero">     
+            <img class="picture" src="img/services.jpg" id="pic" alt="elder couple">
+        <figcaption>
+            <h1>Dịch Vụ</h1>
+        </figcaption>
+    </figure>
+        <h2>Lời Hứa</h2>
         <p>Tại Motherly Home Care, sự an toàn và hài lòng là trên hết. Chúng tôi dành thêm thời gian để đảm bảo các nhu cầu của bạn được đáp ứng, đồng thời còn vượt lên trên để bạn có thể tận hưởng cuộc sống theo cách bạn muốn. Bạn sẽ luôn được đối xử như một gia đình tại Motherly Home Care.</p>
         <h2>Chăm Sóc Yêu Thương</h2>
             <ul>

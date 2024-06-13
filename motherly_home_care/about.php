998516755608f150cec395888ead16f69e74eb3c
Ben bui <btb4516@g.rit.edu>
2024-06-13 15:43:43 -0400

    footer update

--- motherly_home_care/about.php
+++ motherly_home_care/about.php
@@ -1,15 +1,17 @@
 <?php include('nav.php'); ?>
 <main>
-    <div id="englishContent">
-        <h1>About Us</h1>
-        <h2>Our Mission</h2>
-        <p>Your safety and quality care is our mission. Providing our clients with the best health options, we strive to make the quality of life richer, healthier, and happier.</p>
-    </div>
-    <div id="vietnameseContent" style="display: none;">
-        <h1>Về Chúng Tôi</h1>
-        <h2>Nhiệm Vụ Của Chúng Tôi</h2>
-        <p>Chúng tôi hứa sẽ chăm sóc bạn, người bệnh của gia đình bạn, cũng như chính chúng tôi đang chăm sóc cho người thân của mình với lòng kiên nhẫn, thấu hiểu và tôn trọng.
-</p>
+    <div class="wrapper">
+
+        <div id="englishContent">
+            <h1>About Us</h1>
+            <h2>Our Mission</h2>
+            <p>Your safety and quality care is our mission. Providing our clients with the best health options, we strive to make the quality of life richer, healthier, and happier.</p>
+        </div>
+        <div id="vietnameseContent" style="display: none;">
+            <h1>Về Chúng Tôi</h1>
+            <h2>Nhiệm Vụ Của Chúng Tôi</h2>
+            <p>Chúng tôi hứa sẽ chăm sóc bạn, người bệnh của gia đình bạn, cũng như chính chúng tôi đang chăm sóc cho người thân của mình với lòng kiên nhẫn, thấu hiểu và tôn trọng.</p>
+        </div>
     </div>
 </main>
 
--- motherly_home_care/services.php
+++ motherly_home_care/services.php
@@ -1,28 +1,30 @@
 <?php include('nav.php'); ?>
 
 <main>
-    <div id="englishContent">
-        <h1>Our Promise</h1>
-        <p>We promise to care for you as any mother would care for her own: patience, kindness, and respect are our priorities. We aim to help ease the burden of personal care tasks & household chores in order for our clients to maintain independence in the comfort of home. By eliminating these daily challenges, our clients are able to spend more time on the activities that bring them joy and purpose. We believe that a great relationship begins with taking time to get to know each other. That's why we ask our clients about daily habits, interests, spoken languages, and cultural traditions before we match them with one of our caregivers.</p>
-        <h2>Our Services</h2>
-            <ul>
-                <li>Care in the comfort of your home or elsewhere, our custom services are provided in many settings.</li>
-                <li>We can run errands and go to appointments with you, as well as setting reminder for you.</li>
-                <li>We provide one-on-one companionship care and homemaking.</li>
-                <li>Meal preparation and planning is a must whne it comes to your health.</li>
-                <li>We provide care with the utmost respect and compassion.</li>
-    </div>
-    <div id="vietnameseContent" style="display: none;">
-        <h1>Lời Hứa</h1>
-        <p>Tại Motherly Home Care, sự an toàn và hài lòng là trên hết. Chúng tôi dành thêm thời gian để đảm bảo các nhu cầu của bạn được đáp ứng, đồng thời còn vượt lên trên để bạn có thể tận hưởng cuộc sống theo cách bạn muốn. Bạn sẽ luôn được đối xử như một gia đình tại Motherly Home Care.</p>
-        <h2>Chăm Sóc Yêu Thương</h2>
-            <ul>
-                <li>Giúp bạn vào nhà vệ sinh và thay quân áo.</li>
-                <li>Giúp bạn uống thuốc theo chỉ định của bác sĩ điều trị.</li>
-                <li>Theo dõi mạch, huyết áp, thân nhiệt, giấc ngủ.</li>
-                <li>Chăm sóc bữa ăn và dọn dẹp.</li>
-                <li>Trò chuyện, đưa bạn đi bác sĩ, đi dạo, hỗ trợ về mặt tinh thần và xoa bóp những nơi khó chịu</li>
-    </div>
+    <div class="wrapper">
+        <div id="englishContent">
+            <h1>Our Promise</h1>
+            <p>We promise to care for you as any mother would care for her own: patience, kindness, and respect are our priorities. We aim to help ease the burden of personal care tasks & household chores in order for our clients to maintain independence in the comfort of home. By eliminating these daily challenges, our clients are able to spend more time on the activities that bring them joy and purpose. We believe that a great relationship begins with taking time to get to know each other. That's why we ask our clients about daily habits, interests, spoken languages, and cultural traditions before we match them with one of our caregivers.</p>
+            <h2>Our Services</h2>
+                <ul>
+                    <li>Care in the comfort of your home or elsewhere, our custom services are provided in many settings.</li>
+                    <li>We can run errands and go to appointments with you, as well as setting reminder for you.</li>
+                    <li>We provide one-on-one companionship care and homemaking.</li>
+                    <li>Meal preparation and planning is a must whne it comes to your health.</li>
+                    <li>We provide care with the utmost respect and compassion.</li>
+        </div>
+        <div id="vietnameseContent" style="display: none;">
+            <h1>Lời Hứa</h1>
+            <p>Tại Motherly Home Care, sự an toàn và hài lòng là trên hết. Chúng tôi dành thêm thời gian để đảm bảo các nhu cầu của bạn được đáp ứng, đồng thời còn vượt lên trên để bạn có thể tận hưởng cuộc sống theo cách bạn muốn. Bạn sẽ luôn được đối xử như một gia đình tại Motherly Home Care.</p>
+            <h2>Chăm Sóc Yêu Thương</h2>
+                <ul>
+                    <li>Giúp bạn vào nhà vệ sinh và thay quân áo.</li>
+                    <li>Giúp bạn uống thuốc theo chỉ định của bác sĩ điều trị.</li>
+                    <li>Theo dõi mạch, huyết áp, thân nhiệt, giấc ngủ.</li>
+                    <li>Chăm sóc bữa ăn và dọn dẹp.</li>
+                    <li>Trò chuyện, đưa bạn đi bác sĩ, đi dạo, hỗ trợ về mặt tinh thần và xoa bóp những nơi khó chịu</li>
+        </div>
+</div>
 </main>
 
 <?php include('footer.php'); ?>

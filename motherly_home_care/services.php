cb08142873e27b3bd2bad94ba7f3af976ea337a5
Ben bui <btb4516@g.rit.edu>
2024-06-13 16:57:26 -0400

    update service

--- motherly_home_care/services.php
+++ motherly_home_care/services.php
@@ -6,10 +6,29 @@
         <p>We promise to care for you as any mother would care for her own: patience, kindness, and respect are our priorities. We aim to help ease the burden of personal care tasks & household chores in order for our clients to maintain independence in the comfort of home. By eliminating these daily challenges, our clients are able to spend more time on the activities that bring them joy and purpose. We believe that a great relationship begins with taking time to get to know each other. That's why we ask our clients about daily habits, interests, spoken languages, and cultural traditions before we match them with one of our caregivers.</p>
         <h2>Our Services</h2>
             <ul>
+                <figure>
+                <img class="picture" src="img/elder_care2.jpg" id="pic" alt="elder care" >
+                </figure> 
                 <li>Care in the comfort of your home or elsewhere, our custom services are provided in many settings.</li>
+
+                <figure>
+                    <img class="picture" src="img/appointment.jpg" id="pic" alt="appointment" >
+                </figure> 
                 <li>We can run errands and go to appointments with you, as well as setting reminder for you.</li>
+
+                <figure>
+                    <img class="picture" src="img/companion.jpg" id="pic" alt="companion" >
+                </figure> 
                 <li>We provide one-on-one companionship care and homemaking.</li>
+
+                <figure>
+                <img class="picture" src="img/meal_prep.jpg" id="pic" alt="mealprep" >
+                </figure> 
                 <li>Meal preparation and planning is a must whne it comes to your health.</li>
+
+                <figure>
+                    <img class="picture" src="img/elder_care3.jpg" id="pic" alt="elder care" >
+                </figure> 
                 <li>We provide care with the utmost respect and compassion.</li>
     </div>
     <div id="vietnameseContent" style="display: none;">
@@ -17,10 +36,29 @@
         <p>Tại Motherly Home Care, sự an toàn và hài lòng là trên hết. Chúng tôi dành thêm thời gian để đảm bảo các nhu cầu của bạn được đáp ứng, đồng thời còn vượt lên trên để bạn có thể tận hưởng cuộc sống theo cách bạn muốn. Bạn sẽ luôn được đối xử như một gia đình tại Motherly Home Care.</p>
         <h2>Chăm Sóc Yêu Thương</h2>
             <ul>
+                <figure>
+                    <img class="picture" src="img/elder_care2.jpg" id="pic" alt="elder care" >
+                </figure> 
                 <li>Giúp bạn vào nhà vệ sinh và thay quân áo.</li>
+
+                <figure>
+                    <img class="picture" src="img/medicine.jpg" id="pic" alt="medicine" >
+                </figure> 
                 <li>Giúp bạn uống thuốc theo chỉ định của bác sĩ điều trị.</li>
+
+                <figure>
+                <img class="picture" src="img/elder_care4.jpg" id="pic" alt="elder care" >
+                </figure> 
                 <li>Theo dõi mạch, huyết áp, thân nhiệt, giấc ngủ.</li>
+
+                <figure>
+                <img class="picture" src="img/meal_prep.jpg" id="pic" alt="mealprep" >
+                </figure> 
                 <li>Chăm sóc bữa ăn và dọn dẹp.</li>
+
+                <figure>
+                    <img class="picture" src="img/appointment.jpg" id="pic" alt="appointment" >
+                </figure> 
                 <li>Trò chuyện, đưa bạn đi bác sĩ, đi dạo, hỗ trợ về mặt tinh thần và xoa bóp những nơi khó chịu</li>
     </div>
 </main>

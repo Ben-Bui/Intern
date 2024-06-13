eccfa2bd636d6bfd1d2f9426a54b8e6e70bb9ee9
Ben bui <btb4516@g.rit.edu>
2024-06-13 15:59:42 -0400

    update homepage

--- motherly_home_care/index.php
+++ motherly_home_care/index.php
@@ -3,18 +3,34 @@
     <div class="hero">
         <h1>Motherly Home Care</h1></div>
     <div id="englishContent">
-    <h2>When it's family...</h2>
-        <p>Motherly is a home health care agency for adults and seniors living in the Philadelphia area. We offer companionship service and assistance with activities of daily living. This includes dressing, bathing, meal preparation, medication reminders, and many other household tasks as well as other services personally tailored for your care.</p>
-        <h3>For your everyday needs in the comfort of your home, we are here for you.</h3>
-        <p>Motherly is a home health care agency for adults and seniors living in the Philadelphia area. We offer companionship service and assistance with activities of daily living. This includes dressing, bathing, meal preparation, medication reminders, and many other household tasks & errands. Take a look at our full list of Services.<br> At Motherly Home Care, safety & satisfaction come first. We take the extra time to ensure all of your needs are met, and we go above & beyond so you can enjoy life you deserve. You'll always be treated like family here at Motherly Home Care
-        </p>
+        <h2>When it's family...</h2>
+            <p>Motherly is a home health care agency for adults and seniors living in the Philadelphia area. We offer companionship service and assistance with activities of daily living. This includes dressing, bathing, meal preparation, medication reminders, and many other household tasks as well as other services personally tailored for your care.</p>
+            <h3>For your everyday needs in the comfort of your home, we are here for you.</h3>
+            <p>Motherly is a home health care agency for adults and seniors living in the Philadelphia area. We offer companionship service and assistance with activities of daily living. This includes dressing, bathing, meal preparation, medication reminders, and many other household tasks & errands. Take a look at our full list of Services.<br> At Motherly Home Care, safety & satisfaction come first. We take the extra time to ensure all of your needs are met, and we go above & beyond so you can enjoy life you deserve. You'll always be treated like family here at Motherly Home Care
+            </p>
+        <h2>Ask your self!:</h2>
+        <ol>
+            <li>Do you feel limited by your mobility</li>
+            <li>Is there too much worry about your safety?</li>
+            <li>Do you have difficulty speaking</li>
+        </ol>
+        <h2>Why continue wishing for help to do the things you love? WE are here for you.</h2>
+        <p>At Mothery home care we make it our mission to care for senior adults in the comfort of their own home, give us a call for more questions!</p>
     </div>
 
     <div id="vietnameseContent" style="display: none;">
         <h2>Khi là gia đình...</h2>
-        <p>Motherly là một công ty chăm sóc sức khỏe tại nhà cho người lớn và người cao tuổi sống trong khu vực Philadelphia. Chúng tôi cung cấp dịch vụ bạn bè và hỗ trợ với các hoạt động hàng ngày. Điều này bao gồm việc mặc quần áo, tắm, chuẩn bị bữa ăn, nhắc nhở đường dùng thuốc, và nhiều công việc hộ gia đình khác cũng như các dịch vụ khác được cá nhân hóa cho sự chăm sóc của bạn.</p>
-        <h3>Trong sự thoải mái của ngôi nhà của bạn</h3>
-        <p>Chúng tôi đồng hành và hỗ trợ trong các hoạt động sinh hoạt hàng ngày bao gồm thay quần áo, tắm rửa, chuẩn bị bữa ăn, nhắc nhở thuốc men, và nhiều công việc gia đình & việc lặt vặt khác. Hãy xem danh sách đây đủ các Dịch vụ của chúng tôi.</p>
+            <p>Motherly là một công ty chăm sóc sức khỏe tại nhà cho người lớn và người cao tuổi sống trong khu vực Philadelphia. Chúng tôi cung cấp dịch vụ bạn bè và hỗ trợ với các hoạt động hàng ngày. Điều này bao gồm việc mặc quần áo, tắm, chuẩn bị bữa ăn, nhắc nhở đường dùng thuốc, và nhiều công việc hộ gia đình khác cũng như các dịch vụ khác được cá nhân hóa cho sự chăm sóc của bạn.</p>
+        <h2>Trong sự thoải mái của ngôi nhà của bạn</h3>
+            <p>Chúng tôi đồng hành và hỗ trợ trong các hoạt động sinh hoạt hàng ngày bao gồm thay quần áo, tắm rửa, chuẩn bị bữa ăn, nhắc nhở thuốc men, và nhiều công việc gia đình & việc lặt vặt khác. Hãy xem danh sách đây đủ các Dịch vụ của chúng tôi.</p>
+        <h2>Hảy tử hỏi:</h2>
+        <ol>
+            <li>Bạn có cảm thấy hạn chế về khả năng di chuyển của mình?</li>
+            <li>Bạn có lo lắng về sự an toàn của mình</li>
+            <li>Bạn có gặp những cản trở về ngôn ngữ?</li>
+        </ol>
+        <h2>Tại sao tiếp tục mong muốn sự giúp đỡ để làm những điều bạn yêu thích? CHÚNG TÔI ở đây để giúp bạn.</h2>
+        <p>Tại Motherly Home Care, chúng tôi đặt mục tiêu chăm sóc người lớn tuổi trong sự thoải mái của ngôi nhà của họ. Hãy gọi cho chúng tôi nếu bạn có thêm câu hỏi!</p>
     </div>
     </main>
 <?php include('footer.php'); ?>

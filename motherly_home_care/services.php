6fa71abad49300d4e17d965c47f7d7749d73a5cd
Ben bui <btb4516@g.rit.edu>
2024-07-10 17:59:02 -0400

    update service

--- motherly_home_care/services.php
+++ motherly_home_care/services.php
@@ -2,76 +2,71 @@
 
 <main>
     <div id="englishContent">
-    <figure class="hero">     
+        <figure class="hero">     
             <img class="picture" src="img/services.jpg" id="pic" alt="elder couple">
-        <figcaption>
-            <h1>Services</h1>
-        </figcaption>
-    </figure>
+            <figcaption>
+                <h1>Services</h1>
+            </figcaption>
+        </figure>
         <h2>Our Promise</h2>
         <p>We promise to care for you as any mother would care for her own: patience, kindness, and respect are our priorities. We aim to help ease the burden of personal care tasks & household chores in order for our clients to maintain independence in the comfort of home. By eliminating these daily challenges, our clients are able to spend more time on the activities that bring them joy and purpose. We believe that a great relationship begins with taking time to get to know each other. That's why we ask our clients about daily habits, interests, spoken languages, and cultural traditions before we match them with one of our caregivers.</p>
         <h2>Our Services</h2>
-            <ul>
-                <figure>
-                <img class="picture" src="img/elder_care2.jpg" id="pic" alt="elder care" >
-                </figure> 
-                <li>Care in the comfort of your home or elsewhere, our custom services are provided in many settings.</li>
-
-                <figure>
-                    <img class="picture" src="img/appointment.jpg" id="pic" alt="appointment" >
-                </figure> 
-                <li>We can run errands and go to appointments with you, as well as setting reminder for you.</li>
-
-                <figure>
-                    <img class="picture" src="img/companion.jpg" id="pic" alt="companion" >
-                </figure> 
-                <li>We provide one-on-one companionship care and homemaking.</li>
-
-                <figure>
-                <img class="picture" src="img/meal_prep.jpg" id="pic" alt="mealprep" >
-                </figure> 
-                <li>Meal preparation and planning is a must whne it comes to your health.</li>
-
-                <figure>
-                    <img class="picture" src="img/elder_care3.jpg" id="pic" alt="elder care" width="50%" >
-                </figure> 
-                <li>We provide care with the utmost respect and compassion.</li>
+        <div class="services-grid">
+            <div class="service-item">
+                <img class="picture" src="img/elder_care2.jpg" id="pic" alt="elder care">
+                <p>Care in the comfort of your home or elsewhere, our custom services are provided in many settings.</p>
+            </div>
+            <div class="service-item">
+                <img class="picture" src="img/appointment.jpg" id="pic" alt="appointment">
+                <p>We can run errands and go to appointments with you, as well as setting reminder for you.</p>
+            </div>
+            <div class="service-item">
+                <img class="picture" src="img/companion.jpg" id="pic" alt="companion">
+                <p>We provide one-on-one companionship care and homemaking.</p>
+            </div>
+            <div class="service-item">
+                <img class="picture" src="img/meal_prep.jpg" id="pic" alt="mealprep">
+                <p>Meal preparation and planning is a must when it comes to your health.</p>
+            </div>
+            <div class="service-item">
+                <img class="picture" src="img/elder_care3.jpg" id="pic" alt="elder care" width="50%">
+                <p>We provide care with the utmost respect and compassion.</p>
+            </div>
+        </div>
     </div>
+    
     <div id="vietnameseContent" style="display: none;">
-    <figure class="hero">     
+        <figure class="hero">     
             <img class="picture" src="img/services.jpg" id="pic" alt="elder couple">
-        <figcaption>
-            <h1>Dịch Vụ</h1>
-        </figcaption>
-    </figure>
+            <figcaption>
+                <h1>Dịch Vụ</h1>
+            </figcaption>
+        </figure>
         <h2>Lời Hứa</h2>
         <p>Tại Motherly Home Care, sự an toàn và hài lòng là trên hết. Chúng tôi dành thêm thời gian để đảm bảo các nhu cầu của bạn được đáp ứng, đồng thời còn vượt lên trên để bạn có thể tận hưởng cuộc sống theo cách bạn muốn. Bạn sẽ luôn được đối xử như một gia đình tại Motherly Home Care.</p>
         <h2>Chăm Sóc Yêu Thương</h2>
-            <ul>
-                <figure>
-                    <img class="picture" src="img/elder_care2.jpg" id="pic" alt="elder care" >
-                </figure> 
-                <li>Giúp bạn vào nhà vệ sinh và thay quân áo.</li>
-
-                <figure>
-                    <img class="picture" src="img/medicine.jpeg" id="pic" alt="medicine" width="50%">
-                </figure> 
-                <li>Giúp bạn uống thuốc theo chỉ định của bác sĩ điều trị.</li>
-
-                <figure>
-                <img class="picture" src="img/elder_care4.jpg" id="pic" alt="elder care" >
-                </figure> 
-                <li>Theo dõi mạch, huyết áp, thân nhiệt, giấc ngủ.</li>
-
-                <figure>
-                <img class="picture" src="img/meal_prep.jpg" id="pic" alt="mealprep" >
-                </figure> 
-                <li>Chăm sóc bữa ăn và dọn dẹp.</li>
-
-                <figure>
-                    <img class="picture" src="img/appointment.jpg" id="pic" alt="appointment" >
-                </figure> 
-                <li>Trò chuyện, đưa bạn đi bác sĩ, đi dạo, hỗ trợ về mặt tinh thần và xoa bóp những nơi khó chịu</li>
+        <div class="services-grid">
+            <div class="service-item">
+                <img class="picture" src="img/elder_care2.jpg" id="pic" alt="elder care">
+                <p>Giúp bạn vào nhà vệ sinh và thay quân áo.</p>
+            </div>
+            <div class="service-item">
+                <img class="picture" src="img/medicine.jpeg" id="pic" alt="medicine" width="50%">
+                <p>Giúp bạn uống thuốc theo chỉ định của bác sĩ điều trị.</p>
+            </div>
+            <div class="service-item">
+                <img class="picture" src="img/elder_care4.jpg" id="pic" alt="elder care">
+                <p>Theo dõi mạch, huyết áp, thân nhiệt, giấc ngủ.</p>
+            </div>
+            <div class="service-item">
+                <img class="picture" src="img/meal_prep.jpg" id="pic" alt="mealprep">
+                <p>Chăm sóc bữa ăn và dọn dẹp.</p>
+            </div>
+            <div class="service-item">
+                <img class="picture" src="img/appointment.jpg" id="pic" alt="appointment">
+                <p>Trò chuyện, đưa bạn đi bác sĩ, đi dạo, hỗ trợ về mặt tinh thần và xoa bóp những nơi khó chịu.</p>
+            </div>
+        </div>
     </div>
 </main>
 
